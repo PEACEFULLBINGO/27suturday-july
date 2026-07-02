3b241ee8625b2a378ed9c80dbe56a022f58487d8
PEACEFULL <jeswinjubingeorge@gmail.com>
2026-07-02 19:52:04 +0530

    fix(import): normalize Button import in FocusRoomPage

--- src/features/focus-room/views/FocusRoomPage.tsx
+++ src/features/focus-room/views/FocusRoomPage.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/button/Button';
+import { Button } from '@/components/Button/Button';
 import { useFocusTimer } from '../hooks/useFocusTimer';
 
 export function FocusRoomPage() {
